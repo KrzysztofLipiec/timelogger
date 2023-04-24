520bd6c7b2054b917862e6df229bcc1c87af38c8
Krzysztof Lipiec <K.Lipiec@pocztowy.pl>
2023-04-24 15:11:18 +0200

    override

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = {
-    redmineToken: '85573996f04e7c35b1339d8a93e260c2652dc113',
+    redmineToken: '85573********c35b1339d8********2dc113',
     redmineAddress: 'https://bpredmine.bp.com.pl',
     domyslneGodzinyPracy: '9-17',
     domyslneMiejscePracy: 'Łódź',
